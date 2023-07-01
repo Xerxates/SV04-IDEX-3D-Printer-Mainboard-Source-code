bd117aed1f6d47ed7d33c539fa80966aac9e76d5
Patrick <mail@pfritzsche.de>
2023-07-01 23:37:08 +0200

    Fixed min and max position limits for second tool head

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -841,8 +841,8 @@
 #if ENABLED(DUAL_X_CARRIAGE)
   #define X1_MIN_POS X_MIN_POS    // Set to X_MIN_POS
   #define X1_MAX_POS X_BED_SIZE   // A max coordinate so the X1 carriage can't hit the parked X2 carriage
-  #define X2_MIN_POS    82        // A min coordinate so the X2 carriage can't hit the parked X1 carriage
-  #define X2_MAX_POS   353        // The max position of the X2 carriage, typically also the home position
+  #define X2_MIN_POS    0        // A min coordinate so the X2 carriage can't hit the parked X1 carriage
+  #define X2_MAX_POS   362        // The max position of the X2 carriage, typically also the home position
   #define X2_HOME_DIR    1        // Set to 1. The X2 carriage always homes to the max endstop position
   #define X2_HOME_POS X2_MAX_POS  // Default X2 home position. Set to X2_MAX_POS.
                                   // NOTE: For Dual X Carriage use M218 T1 Xn to override the X2_HOME_POS.
